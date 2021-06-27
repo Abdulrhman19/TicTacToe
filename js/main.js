88c62e6e156258e2a0a6067c8518adddc5ee84ff
Abdulrhman19 <am_alharthi19@hotmail.com>
2021-06-27 14:18:28 +0300

    Added Dark/Light modes

--- js/main.js
+++ js/main.js
@@ -4,10 +4,39 @@ import GameView from "./GameView.js";
 let game = new Game();
 let gameView = new GameView();
 let tiles = document.querySelectorAll(".board-tile");
+let toggleBtn = document.querySelector(".toggle-btn");
+
+const title = document.getElementById("title");
+const restart = document.getElementById("restart");
 
 document.querySelector(".restart").addEventListener("click", () => {
     onRestartClick();
 })
+
+
+toggleBtn.addEventListener("click", () => {
+  console.log("clicked");
+  toggleBtn.classList.contains("toggled") 
+    ? enableLightMode()
+    : enableDarkMode();
+
+})
+
+
+const enableLightMode = () => {
+  title.classList.remove("light-title-color");
+  restart.classList.remove("light-restart-button")
+  toggleBtn.classList.remove("toggled");
+  document.body.style.backgroundColor = "#fff";
+}
+
+const enableDarkMode = () => {
+  toggleBtn.classList.add("toggled");
+  document.body.style.backgroundColor = "#121212";
+  title.classList.add("light-title-color");
+  restart.classList.add("light-restart-button")
+}
+
 
 tiles.forEach((tile) => {
   tile.addEventListener("click", () => {
